7f19619384fbc1077e6e379c74744788d4b79fa3
DjoleR <djordje.relic@ymail.com>
2013-07-20 14:18:21 +0200

    Updater: python skripta koja sluzi za dinamicko pravljenje funckija loadDownloadSection() i loadNewsSection() koje dinamicki puno stranicu index.html. Ideja je da se vesti i fajlovi koji ce biti izlozeni na homepage-u ubacuju dinamicki i tako da se zaobidje direktno menjanje .html fajla. Vesti se dodaju u datoteku news.txt u formatu : [redni broj]_[vest]. Koriste se tagovi html standarda za formatiranje teksta. Fajlovi koji se dodaju, moraju biti u PDF formatu, kopiraju se u folder 'files' i u datoteku listaFajlova.txt se dodaju na kraj u formatu: [redni broj]_[ime datoteke]#[naziv koji treba da bude istaknut]. Fajlovi koji nisu stariji od 2 sedmice u trenutku dodavanja poslednjeg fajla ce imat dodatak trepcuceg "new!!!". Da bi rezultat ove skripte imao efekat, trenutno je procedura da se nakon interpretiranja skripte, novo nastali fajl loadDownloadAndNewsSection.js prekopira u folder js u glavnom projetku.

---
+++ Updater/app.py
@@ -0,0 +1,116 @@
+# Problem koji resava ova skripta je sldeci : Ucitava listu PDF fajlova koji koji se
+# nalaze u tekstualnom fajlu listaFajlova.txt. Svaki od fajlova mora biti imenovan u 
+# formatu [redni broj]_[naziv fajla].pdf
+# Pri interpetiranju skripte, bice ucitani svi fajlovi, ekstrakovani njihovi nazivi
+# i bice generisan tekstualni dokument sa ekstenzijom .js koji ce pretstavljati skriptu
+# koja ce dinamicki da da popuni index.html dokument sa poslednjim fajlovima koji ce
+# biti slobodni za download.
+
+
+#######################################
+#        Updater za DownlaodSection
+#######################################
+
+# Ucitavamo fajl listaFajlova.txt. I ukoliko dodje do neuspesnog ucitavanja  izbacujemo
+# gresku
+try:
+    f = open("listaFajlova.txt", "r")
+except (IOError):
+    print "Error opening file. 'listaFajlova.txt'"
+    exit()
+
+# U niz fajlovi sam ucitao sve navedene fajlove (svaki se navodi jedan ispod drugog    
+linijeDownload = f.readlines()
+
+# Iterativno prolazim kroz sve fajlove i izvlacim naziv pdf fajla i ime koje ce mu biti prikazano
+# u nizove fajlovi i imena
+
+fajlovi = []
+imena = []
+for i in range(0, len(linijeDownload)):
+    fajlovi.append(linijeDownload[i][linijeDownload[i].index('_') + 1 : linijeDownload[i].index('#')])
+    imena.append(linijeDownload[i][linijeDownload[i].index('#') + 1: linijeDownload[i].index('.')])
+
+# Otvaramo datoteku loadNewsAndDownloadSection.js za upisivanje i u nju upisujemo
+# tagove i uputstva za popunjavanje News i Dowload sekcije na index strani sajta
+try:
+    g = open("loadNewsAndDownloadSection.js", "w")
+except (IOError):
+    print "Error opening file 'loadNewsAndDownloadSection.js'."
+
+# Upisujem loadNewsAndDownloadSection.js kod koji ce dinamicki da popuni index.html stranu po njenom ucitavanju sa poslednje
+# dodatim fajlovima za download. Ukoliko postoje fajlovi koji su dodati u poslednje 2 nedelje (1209600 sec) tada ce se uz 
+# fajl dodati trepcuce "new" pored. Za to su potrebni paketi os i datetime. NAPOMENA: IE ne podrzava blink mogucnost
+ 
+import os, datetime
+from time import time
+
+blinkFeature = "    <blink style = 'color: #1D5377; text-decoration: blink'>new!!!</blink>"
+
+g.write("function loadDownloadSection(){\n")
+
+# Dodajem poslednji fajl na vrh sa dodatkom trepcuceg "new!!!" posto je to poslednji dodat fajl
+g.write("document.getElementById(\"DownloadSection\").innerHTML = \"<li><a href ='../files/" + fajlovi[len(fajlovi)-1] +".pdf'><img src = 'images/pdf-icon2.png' height='17px' style='margin-left: 5px; margin-top: 3px; margin-right: 10px'></a>" + imena[len(imena)-1] + blinkFeature + "</li>")
+
+# Proveravam vreme kreiranja fajla koji je pretposlednji dodat. Ukoliko je on dodat pre manje od 2 nedelje, uz njega ce da stoji
+# dodato trepcuce "new!!!". Vreme kreiranja fajla proveravam preko fje os.path.getmtime('imefajla.pdf')
+# link ka referenci -> http://stackoverflow.com/questions/415511/how-to-get-current-time-in-python
+# trenutno vreme dobijam preko funkcije : time()
+# Treba dodati try-except kalusule i napraviti program robusnim. Takodje treba obratiti paznju gde se nalaze
+# fajlovi koji su trazeni, kao i na to gde ce program biti instaliran tj sa koje lokacije ce se pokretati
+
+if (time() - os.path.getmtime("files/" + fajlovi[len(fajlovi) - 2] + ".pdf") < 1209600):
+    g.write("<li><a href ='../files/" + fajlovi[len(fajlovi)-2] +".pdf'><img src = 'images/pdf-icon2.png' height='17px' style='margin-left: 5px; margin-top: 3px; margin-right: 10px'></a>" + imena[len(imena)-2] + blinkFeature +  "</li>")
+else:
+    g.write("<li><a href ='../files/" + fajlovi[len(fajlovi)-2] +".pdf'><img src = 'images/pdf-icon2.png' height='17px' style='margin-left: 5px; margin-top: 3px; margin-right: 10px'></a>" + imena[len(imena)-2] + "</li>")
+
+if (time() - os.path.getmtime("files/" + fajlovi[len(fajlovi) - 3] + ".pdf") < 1209600):
+    g.write("<li><a href ='../files/" + fajlovi[len(fajlovi)-3] +".pdf'><img src = 'images/pdf-icon2.png' height='17px' style='margin-left: 5px; margin-top: 3px; margin-right: 10px'></a>" + imena[len(imena)-3] + blinkFeature + "</li>\"")
+else:
+    g.write("<li><a href ='../files/" + fajlovi[len(fajlovi)-3] +".pdf'><img src = 'images/pdf-icon2.png' height='17px' style='margin-left: 5px; margin-top: 3px; margin-right: 10px'></a>" + imena[len(imena)-3] + "</li>\"")
+
+g.write(";\n")
+g.write("}\n\n")
+
+
+# Zatvaram fajlove listaFajlova.txt posto sam uradio sa njim sve sto mi je trebalo
+# datoteku g ostavljam otvorenu zato sto cu u nju dodati jos funkciju za ucitavanje News sekcije
+f.close()
+
+    
+#######################################
+#        Updater za NewsSection
+#######################################
+
+# Ucitavamo fajl news.txt. I ukoliko dodje do neuspesnog ucitavanja  izbacujemo
+# gresku
+try:
+    h = open("news.txt", "r")
+except (IOError):
+    print "Error opening file. 'news.txt'"
+    exit()
+    
+# U niz fajlovi sam ucitao sve navedene vesti, navode se jedna ispod druge sa vodecim rednim brojem
+# format : [redni broj]_[vest]    
+linijeNews = h.readlines()
+
+# Iterativno prolazim kroz sve linije u fajlu i izvlacim vesti i smestam ih u niz vesti
+# Izlvacenje se vrsi tako sto izostavljam vodece redne brojeve i donju crtu ispod
+vesti = []
+for i in range(0, len(linijeNews)):
+    vesti.append(linijeNews[i][linijeNews[i].index('_') + 1 : -1])
+
+# U datoteci kreiram funkciju loadNewsSection() koja ce imati komande za dimanicko punjenje stranice index.html
+# Bice samo poslednjih 6 vesti ukljuceno!!!
+
+g.write("function loadNewsSection(){\n")
+
+# Ubacujemo poslednjih 6 vesti, prvo posledju pa preostalih 5
+
+for i in range(0,6):
+    g.write("document.getElementById(\"NewsSection" + str(i) + "\").innerHTML = \"" + vesti[len(vesti)-(i+1)] + "\";\n")
+
+g.write("}")
+
+h.close()
+g.close()
---
+++ Updater/loadNewsAndDownloadSection.js
@@ -0,0 +1,12 @@
+function loadDownloadSection(){
+document.getElementById("DownloadSection").innerHTML = "<li><a href ='../files/nz1.pdf'><img src = 'images/pdf-icon2.png' height='17px' style='margin-left: 5px; margin-top: 3px; margin-right: 10px'></a>NZ-1    <blink style = 'color: #1D5377; text-decoration: blink'>new!!!</blink></li><li><a href ='../files/mun.pdf'><img src = 'images/pdf-icon2.png' height='17px' style='margin-left: 5px; margin-top: 3px; margin-right: 10px'></a>M-UN    <blink style = 'color: #1D5377; text-decoration: blink'>new!!!</blink></li><li><a href ='../files/m4k.pdf'><img src = 'images/pdf-icon2.png' height='17px' style='margin-left: 5px; margin-top: 3px; margin-right: 10px'></a>M4K    <blink style = 'color: #1D5377; text-decoration: blink'>new!!!</blink></li>";
+}
+
+function loadNewsSection(){
+document.getElementById("NewsSection0").innerHTML = "<strong>Poslednja vest: </strong>Ovaj news updater bi trebao da radi!!! July 20th 201";
+document.getElementById("NewsSection1").innerHTML = "<strong>Press Release:</strong>Far Eastern University Takes Higher Education to the Cloud with NetSuite";
+document.getElementById("NewsSection2").innerHTML = "<strong>Press Release:</strong>NetSuite Announces Timing of Second Quarter 2013 Financial Results";
+document.getElementById("NewsSection3").innerHTML = "<strong>White Paper:</strong>The Benefits of Running Your Entire Business on One System";
+document.getElementById("NewsSection4").innerHTML = "<strong>Live Product Demo: </strong>NetSuite for Advertising Companies July 16, 2013";
+document.getElementById("NewsSection5").innerHTML = "<strong>Webinar: Hype or Not: </strong>Moving Manufacturing to the Cloud July 11, 2013";
+}
